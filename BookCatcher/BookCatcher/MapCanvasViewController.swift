940635725543c1118ac59e862a51b396a1785c98
Jorge Rebollo <jorgerebolloj@gmail.com>
2017-06-07 19:41:16 -0500

    CoreLocation implemented

--- BookCatcher/BookCatcher/MapCanvasViewController.swift
+++ BookCatcher/BookCatcher/MapCanvasViewController.swift
@@ -9,19 +9,37 @@
 import UIKit
 import MapKit
 
-class MapCanvasViewController: UIViewController {
+class MapCanvasViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var canvasMapView: MKMapView!
+    
+    var manager = CLLocationManager()
+    var updateLocationCount = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        manager.delegate = self
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            canvasMapView.showsUserLocation = true
+            manager.startUpdatingLocation()
+        } else {
+            manager.requestWhenInUseAuthorization()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: CLLocationManagerDelegate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if updateLocationCount < 4 {
+            let region = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, 1000, 1000)
+            canvasMapView.setRegion(region, animated: true)
+        } else {
+            manager.stopUpdatingLocation()
+        }
+        
+    }
 }
 
